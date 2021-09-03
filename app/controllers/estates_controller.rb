06ee4cc1d775f44807715947e4416fde158b1059
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-09-03 17:25:13 +0900

    updateアクション修正、不要ファイル削除

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -1,28 +1,23 @@
 class EstatesController < ApplicationController
   before_action :set_estate, only: %i[ show edit update destroy ]
 
-  # GET /estates or /estates.json
   def index
     @estates = Estate.all
   end
 
-  # GET /estates/1 or /estates/1.json
   def show
     @near_stations = @estate.near_stations
   end
 
-  # GET /estates/new
   def new
     @estate = Estate.new
     2.times { @estate.near_stations.build }
   end
 
-  # GET /estates/1/edit
   def edit
     @estate.near_stations.build
   end
 
-  # POST /estates or /estates.json
   def create
     @estate = Estate.new(estate_params)
     if @estate.save
@@ -33,18 +28,14 @@ class EstatesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /estates/1 or /estates/1.json
   def update
-    respond_to do |format|
-      if @estate.update(estate_params)
-        redirect_to estates_path, notice: "情報を編集しました！"
-      else
-        render :edit
-      end
+    if @estate.update(estate_params)
+      redirect_to estates_path, notice: "情報を編集しました！"
+    else
+      render :edit
     end
   end
 
-  # DELETE /estates/1 or /estates/1.json
   def destroy
     @estate.destroy
     respond_to do |format|
@@ -55,12 +46,10 @@ class EstatesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_estate
     @estate = Estate.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def estate_params
     params.require(:estate).permit(:name, :price, :adress, :age, :content, near_stations_attributes: [:line, :station_name, :time_on_foot, :estate_id])
   end
--- app/views/near_stations/_near_station.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! near_station, :id, :line, :station_name, :time_on_foot, :created_at, :updated_at
-json.url near_station_url(near_station, format: :json)
